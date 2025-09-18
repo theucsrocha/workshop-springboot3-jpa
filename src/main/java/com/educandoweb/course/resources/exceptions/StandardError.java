90f1383abae2dc19eee9054a75da14bfc9e733a8
Matheus Rocha <theucsrocha@gmail.com>
2025-09-18 15:04:26 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
@@ -0,0 +1,71 @@
+package com.educandoweb.course.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Instant timesteamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+		
+	}
+
+	public StandardError(Instant timesteamp, Integer status, String error, String message, String path) {
+		super();
+		this.timesteamp = timesteamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Instant getTimesteamp() {
+		return timesteamp;
+	}
+
+	public void setTimesteamp(Instant timesteamp) {
+		this.timesteamp = timesteamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+	
+}
